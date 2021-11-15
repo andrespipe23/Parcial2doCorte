4ce4ac89c9104161a23f43f5c0b1cd2a74dfe718
Andres <andresfelipe23072000@gmail.com>
2021-11-15 10:25:21 -0500

    Versión 2

--- src/parcial2docorte/MainParcial2doCorte.java
+++ src/parcial2docorte/MainParcial2doCorte.java
@@ -15,7 +15,7 @@ public class MainParcial2doCorte {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Versión 1");
     }
     
 }
